af38bb4ed8d91a8408e2de88c01f3b8f1168865b
= <lukehoogenboom@gmail.com>
2024-07-22 14:38:56 +0200

    test

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,6 @@
 let perms = document.getElementById('listOfPermissions');
 
 perms.addEventListener('itemSelected', function () {
-    alert()
     try {
     window.AppInventor.setWebViewString(perms.getAttribute('value'));
     } catch (e) {}

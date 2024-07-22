968d9ccf6267ec1acd5e592c2ebf8782fe260067
= <lukehoogenboom@gmail.com>
2024-07-22 17:59:04 +0200

    test

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,17 @@
 let perms = document.getElementById('listOfPermissions');
 
+let writePerm = document.getElementById('writeSettings');
+
 perms.addEventListener('itemSelected', function () {
     try {
     window.AppInventor.setWebViewString(perms.getAttribute('value'));
     } catch (e) {}
 });
+
+window.setInterval(function () { // checks for any messages sent from the app.
+    try {
+        if(window.AppInventor.getWEbViewString()['canWriteSettings']) {
+            writePerm.setAttriute('disabled', true);
+        }
+    } catch(e) {}
+},1);

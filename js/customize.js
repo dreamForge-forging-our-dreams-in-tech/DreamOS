9d8895028eb89f622197e19a6cac843c23e0477e
= <lukehoogenboom@gmail.com>
2024-08-12 17:36:05 +0200

    test

--- js/customize.js
+++ js/customize.js
@@ -33,9 +33,17 @@ async function setWallpaper() {
     let fileHandle;
 
     const pickerOpts = {
+        types: [
+          {
+            description: "Images",
+            accept: {
+              "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+            },
+          },
+        ],
         excludeAcceptAllOption: true,
         multiple: false,
-    };
+      };
 
     // open file picker, destructure the one element returned array
     fileHandle = await window.showOpenFilePicker(pickerOpts);

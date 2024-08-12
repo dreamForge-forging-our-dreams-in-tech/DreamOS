96bf4fa37c793b1e517c989b754ea76917c56121
= <lukehoogenboom@gmail.com>
2024-08-12 17:12:53 +0200

    test

---
+++ js/customize.js
@@ -0,0 +1,31 @@
+let perms = document.getElementById('listOfPermissions');
+
+let reset = document.getElementById('reset');
+
+
+perms.addEventListener('itemSelected', function () {
+    try {
+        if(perms.getAttribute('value') == 'customize') {
+            window.location.href = '../html/Customize.html';
+        }
+
+        window.AppInventor.setWebViewString(perms.getAttribute('value'));
+    } catch (e) { }
+});
+
+let int = window.setInterval(function () { // checks for any messages sent from the app.
+    try {
+        console.log(window.AppInventor.getWebViewString())
+        if (JSON.parse(window.AppInventor.getWebViewString())['canWriteSettings']) {
+            clearInterval(int);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}, 1000);
+
+reset.addEventListener('click', function () {
+    try {
+            window.AppInventor.setWebViewString('restart');
+    } catch (e) { }
+});
